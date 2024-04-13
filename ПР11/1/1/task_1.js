5580b4cf9fc4f2b56e9de1d20bbb522f180d67f5
Max9261 <162316493+Max9261@users.noreply.github.com>
2024-04-13 16:35:41 +0900

    Update task_1.js

--- ПР11/1/1/task_1.js
+++ ПР11/1/1/task_1.js
@@ -1,18 +1,1 @@
-const body = document.querySelector('body');
 
-const formHtml = `
-<form class="create-user-form">
-  <label>
-    Имя
-    <input type="text" name="userName" placeholder="Введите ваше имя">
-  </label>
-  <label>
-    Пароль
-    <input type="password" name="password" placeholder="Придумайте пароль">
-  </label>
-  <button type="submit">
-    Подтвердить
-  </button>
-</form>`;
-
-body.innerHTML += formHtml;
