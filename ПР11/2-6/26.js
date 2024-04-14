b20fb6d9f548da71e416678eaa8468ec5c19d0bd
Max9261 <162316493+Max9261@users.noreply.github.com>
2024-04-14 19:24:44 +0900

    Update 26.js

--- ПР11/2-6/26.js
+++ ПР11/2-6/26.js
@@ -1,21 +1,42 @@
 const tasksList = document.querySelector('.tasks-list');
-const taskTemplate = document.getElementById('task-template').innerHTML;
 
 tasks.forEach(task => {
-    const taskElement = document.createElement('li');
-    taskElement.classList.add('task-item');
-    taskElement.innerHTML = taskTemplate;
-
-    const taskCheckbox = taskElement.querySelector('input[type="checkbox"]');
-    taskCheckbox.id = `task-${task.id}`;
-    taskCheckbox.dataset.taskId = task.id;
-    taskCheckbox.checked = task.completed;
-
-    const taskLabel = taskElement.querySelector('label');
-    taskLabel.htmlFor = `task-${task.id}`;
-
-    const taskText = taskElement.querySelector('.task-item__text');
-    taskText.textContent = task.text;
-
-    tasksList.appendChild(taskElement);
+  const taskItem = document.createElement('div');
+  taskItem.classList.add('task-item');
+  taskItem.setAttribute('data-task-id', task.id);
+  
+  const mainContainer = document.createElement('div');
+  mainContainer.classList.add('task-item__main-container');
+  
+  const mainContent = document.createElement('div');
+  mainContent.classList.add('task-item__main-content');
+  
+  const form = document.createElement('form');
+  form.classList.add('checkbox-form');
+  
+  const checkbox = document.createElement('input');
+  checkbox.classList.add('checkbox-form__checkbox');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('id', `task-${task.id}`);
+  
+  const label = document.createElement('label');
+  label.setAttribute('for', `task-${task.id}`);
+  
+  const text = document.createElement('span');
+  text.classList.add('task-item__text');
+  text.textContent = task.text;
+  
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('task-item__delete-button', 'default-button', 'delete-button');
+  deleteButton.setAttribute('data-delete-task-id', task.id);
+  deleteButton.textContent = 'Удалить';
+  
+  tasksList.appendChild(taskItem);
+  taskItem.appendChild(mainContainer);
+  mainContainer.appendChild(mainContent);
+  mainContent.appendChild(form);
+  form.appendChild(checkbox);
+  form.appendChild(label);
+  mainContent.appendChild(text);
+  taskItem.appendChild(deleteButton);
 });
